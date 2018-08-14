e9ff760bed26113ddbf34eef75281331acced18c
Valerie Osei <valerie.osei@gmail.com>
2018-08-14 14:01:04 -0400

    added example form

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -2,4 +2,19 @@ class ExampleController < ApplicationController
   def form
     @job_application = JobApplication.new
   end
+
+  def create
+    @job_application = JobApplication.new
+    @job_application.name = params[:job_application][:name]
+    @job_application.phone = params[:job_application][:phone]
+    @job_application.hobby = params[:job_application][:hobby]
+    @job_application.years_of_experience = params[:job_application][:years_of_experience]
+    @job_application.available_date = params[:job_application][:available_date]
+
+    if @job_application.save
+      redirect_to locations_path
+    else
+      redirect_to new_location_path
+    end
+  end
 end
